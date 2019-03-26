c209f14b28af7a9e76c004f3cfafff3a42ca8975
Admin <admin@Admins-MacBook-Pro.local>
2019-03-26 14:43:11 +0530

    added pre build script android

---
+++ EnvironmentVaribles.Android/appcenter-pre-build.sh
@@ -0,0 +1,16 @@
+﻿#!/usr/bin/env bash
+
+echo "Arguments for updating:"
+echo " - LabelText: $LabelText"
+
+# Updating ids
+
+IdFile=$BUILD_REPOSITORY_LOCALPATH/EnvironmentVaribles/MainPage.xaml.cs
+
+sed -i '' "s/APP_SECRET/$APP_SECRET/g" $IdFile
+
+# Print out file for reference
+cat $IdFile
+
+
+echo "Updated id!"
