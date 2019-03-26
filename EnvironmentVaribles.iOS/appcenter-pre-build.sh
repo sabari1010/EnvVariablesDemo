ff393b29029251919e3aaf6eb4399362b150259b
Admin <admin@rf-030830774553.online.rf.se>
2019-03-26 16:29:08 +0530

    added pre build script ios

---
+++ EnvironmentVaribles.iOS/appcenter-pre-build.sh
@@ -0,0 +1,16 @@
+﻿#!/usr/bin/env bash
+
+echo "Arguments for updating:"
+echo " - LabelText: $LabelText"
+
+# Updating ids
+
+IdFile=$BUILD_REPOSITORY_LOCALPATH/EnvironmentVaribles/MainPage.xaml.cs
+
+sed -i '' "s/LabelText/$LabelText/g" $IdFile
+
+# Print out file for reference
+cat $IdFile
+
+
+echo "Updated id!"
